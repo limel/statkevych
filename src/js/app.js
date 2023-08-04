2c1d1fb7781998d94f7932892f420c0e64018543
Andrey Nazarenko <limelaudio@gmail.com>
2023-08-04 15:31:53 +0300

    update shadow & hover

--- src/js/app.js
+++ src/js/app.js
@@ -3,7 +3,7 @@ import * as baseFunctions from './modules/functions.js';
 document.addEventListener('DOMContentLoaded', () => {
 	// Custom JS
 	baseFunctions.isWebp();
-	baseFunctions.fixedHeader();
+	// baseFunctions.fixedHeader();
 
 	window.addEventListener('scroll', baseFunctions.fixedHeader);
 
