b4a64bf5d222c2cb57fce07934515cdda28bb45f
Andrey Nazarenko <limelaudio@gmail.com>
2023-08-04 17:51:47 +0300

    update hovers & header button sizes

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,13 @@
 import * as baseFunctions from './modules/functions.js';
+import Swiper from 'swiper';
 
 document.addEventListener('DOMContentLoaded', () => {
 	// Custom JS
 	baseFunctions.isWebp();
-	// baseFunctions.fixedHeader();
-
+	const swiper = new Swiper('.swiper', {
+		// Optional parameters
+		loop: true,
+	});
 	window.addEventListener('scroll', baseFunctions.fixedHeader);
 
 	const buttons = document.getElementsByTagName('button');
