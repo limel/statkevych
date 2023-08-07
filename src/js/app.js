08a4f5b0d483ea8461755c76b8959a2f80e2fa7a
Andrey Nazarenko <limelaudio@gmail.com>
2023-08-07 23:42:55 +0300

    add sections

--- src/js/app.js
+++ src/js/app.js
@@ -4,13 +4,25 @@ import Swiper from 'swiper';
 document.addEventListener('DOMContentLoaded', () => {
 	// Custom JS
 	baseFunctions.isWebp();
-	const swiper = new Swiper('.swiper', {
+	const servicesSwiper = new Swiper('.swiper', {
 		// Optional parameters
 		loop: true,
+		slidesPerView: 2,
+		spaceBetween: 32,
 	});
+
+	const prev = document.querySelector('.prev-button');
+	const next = document.querySelector('.next-button');
+
+	prev.addEventListener('click', () => servicesSwiper.slidePrev());
+	next.addEventListener('click', () => servicesSwiper.slideNext());
+
 	window.addEventListener('scroll', baseFunctions.fixedHeader);
 
-	const buttons = document.getElementsByTagName('button');
+	const buttons = document.querySelectorAll(
+		'button:not(.carousel-navigation__button)'
+	);
+
 	for (const button of buttons) {
 		button.addEventListener('click', event =>
 			baseFunctions.createRipple(event)
