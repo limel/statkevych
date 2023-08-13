2d07c33a13898efb210f218bc1b8a85851f4b2ae
Andrey Nazarenko <limelaudio@gmail.com>
2023-08-14 00:09:40 +0300

    add blocks

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,15 @@
 import * as baseFunctions from './modules/functions.js';
 import * as carousels from './modules/carousels.js';
+// import Inputmask from 'inputmask';
 
+// export function inputMask() {
+// 	const phoneInputs = document.querySelectorAll('input[type="tel"]');
+// 	const im = new Inputmask('+380 (99) 99 99 999');
+
+// 	for (const phoneInput of phoneInputs) {
+// 		im.mask(phoneInput);
+// 	}
+// }
 document.addEventListener('DOMContentLoaded', () => {
 	initPage();
 	carousels.initServicesSwiper();
@@ -11,5 +20,6 @@ document.addEventListener('DOMContentLoaded', () => {
 function initPage() {
 	baseFunctions.isWebp();
 	baseFunctions.handlerButtonClick();
+	// inputMask();
 	window.addEventListener('scroll', baseFunctions.fixedHeader);
 }
