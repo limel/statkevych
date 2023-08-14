610c5896c00e316539f5e36f065af36ff4112d5b
Andrey Nazarenko <limelaudio@gmail.com>
2023-08-14 16:03:17 +0300

    update hero add service page

--- src/js/app.js
+++ src/js/app.js
@@ -1,15 +1,6 @@
 import * as baseFunctions from './modules/functions.js';
 import * as carousels from './modules/carousels.js';
-// import Inputmask from 'inputmask';
 
-// export function inputMask() {
-// 	const phoneInputs = document.querySelectorAll('input[type="tel"]');
-// 	const im = new Inputmask('+380 (99) 99 99 999');
-
-// 	for (const phoneInput of phoneInputs) {
-// 		im.mask(phoneInput);
-// 	}
-// }
 document.addEventListener('DOMContentLoaded', () => {
 	initPage();
 	carousels.initServicesSwiper();
@@ -20,6 +11,5 @@ document.addEventListener('DOMContentLoaded', () => {
 function initPage() {
 	baseFunctions.isWebp();
 	baseFunctions.handlerButtonClick();
-	// inputMask();
 	window.addEventListener('scroll', baseFunctions.fixedHeader);
 }
